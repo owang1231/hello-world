050751efaa7a1402fc54feeae966777e8d04f62e
owang1231 <73269326+owang1231@users.noreply.github.com>
2021-02-27 00:10:34 -0700

    Update GitDemo (1).java
    Added name
    returned num and previousNumber sum in addNumber()
    added new variable and reverseNum=newNumber statement in reverseNum() method

--- GitDemo (1).java
+++ GitDemo (1).java
@@ -1,10 +1,10 @@
 /*****
- * Edit this line to have your name.
+Olivia Wang
  ******/
 public class GitDemo {
     
     private double previousNumber;
-    
+    private double reverseNum;
     public GitDemo() {
         previousNumber = 1;
     }
@@ -13,8 +13,9 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
+        double sum= num+previousNumber;
         System.out.print("Add num to previousNumber");
-        return 0;
+        return sum;
     }
     
     /*****
@@ -45,7 +46,7 @@ public class GitDemo {
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
         }
-        
+        reverseNum= newNumber;
         return newNumber;
     }
     
